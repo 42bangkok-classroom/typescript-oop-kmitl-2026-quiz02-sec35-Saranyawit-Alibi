d936980d4484b09411550a24d2224a9185fd1ed4
silk <sanranyywitch15@gmail.com>
2026-01-16 10:20:22 +0700

    quiz2

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,1 +1,13 @@
-export function getEdgePosts() {}
+import axios from 'axios'
+
+
+export function getEdgePosts() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+     .then(response => {
+        console.log(response.data); 
+      })
+      .catch(error => {
+        console.error(error); 
+      });
+ 
+}
